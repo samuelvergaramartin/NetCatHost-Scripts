fe8dd1f691711d7bb221228cac9bec2ba5f33228
Sam170703dev <91561846+samuelvergaramartin@users.noreply.github.com>
2024-01-20 22:04:33 +0100

    Updated start.sh

--- start.sh
+++ start.sh
@@ -50,7 +50,7 @@ echo " "
 echo -e "\e[33m Buscando el archivo de arranque en el /home/container... \e[0m"
 sleep 2
 
-if [[ -f  "${MAIN_FILE}" ]]; then
+if [[ -f  "/home/container${MAIN_FILE}" ]]; then
 	echo -e "\e[32m ✔️ | Detectado: /home/container/${MAIN_FILE} \e[0m"
     sleep 2
     echo -e "\e[33m Cargando /home/container/${MAIN_FILE}... \e[0m"
