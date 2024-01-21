2afa267190a31443ffc301239eaa7c9f2ef91dfa
Sam170703dev <91561846+samuelvergaramartin@users.noreply.github.com>
2024-01-21 21:33:17 +0100

    Update start-mc.sh

--- start-mc.sh
+++ start-mc.sh
@@ -2,7 +2,8 @@
 
 clear
 cd /home/container
-sleep 2
+echo "WAITING..."
+sleep 100
 
 echo -e ".                \e[35m /\_/\ \e[0m \e[31m  | \  |  ___  ____   ___   ___    ____   |    |  ____    ____   ____  \e[0m "
 echo -e ".                \e[35m( o.o )\e[0m \e[31m  |  \ | |___   |    |     |___|    |     |----| |    |  |____    |    \e[0m "
